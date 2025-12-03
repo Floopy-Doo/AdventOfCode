9933055993e7e59ff01587336f39bb05710108c0
Floopy-Doo <florian.bucher@hotmail.com>
2025-12-03 16:19:08 +0100

    Advent of Code 2025 - Refactor for reuse of input logic

---
+++ AoC.2025.Facts/InputParserTest.cs
@@ -0,0 +1,22 @@
+﻿using AcC._2025;
+using Shouldly;
+
+namespace AoC._2025.Facts;
+
+public class InputParserTest
+{
+    public static TheoryData<string, Operation> OperationParseData = new()
+    {
+        { "L68", new Operation.Left(68) },
+        { "L30", new Operation.Left(30) },
+        { "R60", new Operation.Right(60) },
+    };
+
+    [Theory]
+    [MemberData(nameof(OperationParseData))]
+    public void ShouldExtractCorrectOperation(string input, Operation expected)
+    {
+        var result = InputParser.Parse(input);
+        result.ShouldBe(expected);
+    }
+}
---
+++ AoC.2025/InputParser.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AcC._2025;
+
+public static partial class InputParser
+{
+    public static Operation Parse(string action)
+    {
+        var result = ActionRegex().Match(action);
+        if (!result.Success) throw new InvalidOperationException($"Unable to parse {action}");
+
+        var direction = result.Groups[1].Value;
+        var distance = result.Groups[2].Value;
+
+        return direction switch
+        {
+            "R" => new Operation.Right(int.Parse(distance)),
+            "L" => new Operation.Left(int.Parse(distance)),
+            _ => throw new InvalidOperationException($"Unknown action {direction} - {distance}"),
+        };
+    }
+
+
+    [GeneratedRegex(@"^([RL])(\d+)$")]
+    private static partial Regex ActionRegex();
+}
+
+public abstract record Operation
+{
+    public sealed record Left(int Value) : Operation;
+
+    public sealed record Right(int Value) : Operation;
+}
