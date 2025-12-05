4a2392ae081c05e30502bc0f045f6eb7b3472f98
Floopy-Doo <florian.bucher@hotmail.com>
2025-12-05 16:41:56 +0100

    Advent of Code 2025 - Day 3-2

---
+++ AoC.2025.Facts/Day3Part2Tests.cs
@@ -0,0 +1,39 @@
+﻿using Shouldly;
+using static AcC._2025.Day3Part2;
+
+namespace AoC._2025.Facts;
+
+public class Day3Part2Tests
+{
+    public static TheoryData<string, decimal> LineData = new()
+    {
+        { "987654321111111", 987654321111m },
+        { "811111111111119", 811111111119m },
+        { "234234234234278", 434234234278m },
+        { "818181911112111", 888911112111m },
+    };
+
+    [Theory]
+    [MemberData(nameof(LineData))]
+    public void SingleLineSolveData(string input, decimal expected)
+    {
+        var result = Solve(input);
+        result.ShouldBe(expected);
+    }
+
+
+    [Fact]
+    public void ShouldSolveSample()
+    {
+        var input = "987654321111111\n811111111111119\n234234234234278\n818181911112111\n";
+        var result = Solve(input);
+        result.ShouldBe(3121910778619m);
+    }
+
+    [Fact]
+    public void ShouldDay3Input()
+    {
+        var result = Solve(AocInput.Day3Input);
+        result.ShouldBe(169077317650774m);
+    }
+}
---
+++ AoC.2025/Day3Part2.cs
@@ -0,0 +1,46 @@
+﻿namespace AcC._2025;
+
+public static class Day3Part2
+{
+    public static decimal Solve(string input)
+    {
+        return input.Split("\n", StringSplitOptions.RemoveEmptyEntries)
+            .Select(Parse)
+            .Select(SolveLine)
+            .Sum();
+    }
+
+    private static decimal SolveLine(Battery[] line)
+    {
+        var batteries = SolveRecursive(12, line);
+
+        var finalNumber = batteries
+            .OrderBy(x => x.Position)
+            .Aggregate(string.Empty, (acc, battery) => $"{acc}{battery.Jolt}");
+
+        return decimal.Parse(finalNumber);
+    }
+
+    private static IEnumerable<Battery> SolveRecursive(int sampleSize, Battery[] remaining)
+    {
+        if (remaining.Length == 0 || sampleSize == 0) return [];
+
+        var selectionSize = remaining.Length - sampleSize + 1;
+        var selection = remaining
+            .Select((x, i) => (Data: x, Index: i))
+            .Take(selectionSize);
+        var highestBatteryInSample = selection
+            .OrderByDescending(x => x.Data.Jolt)
+            .First();
+        var otherHighest = SolveRecursive(sampleSize - 1, remaining.Skip(highestBatteryInSample.Index + 1).ToArray());
+
+        return otherHighest.Append(highestBatteryInSample.Data);
+    }
+
+    private static Battery[] Parse(string line)
+    {
+        return line.Select((jolt, index) => new Battery(decimal.Parse([jolt]), index)).ToArray();
+    }
+
+    private record Battery(decimal Jolt, int Position);
+}
