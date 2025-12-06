cd2128fb9c8946dc9e7cdd2f7c988d78f2e1369e
Floopy-Doo <florian.bucher@hotmail.com>
2025-12-06 11:30:08 +0100

    Advent of Code 2025 - Day 2-2

--- AoC.2025.Facts/Day2Test.cs
+++ AoC.2025.Facts/Day2Test.cs
@@ -5,7 +5,7 @@ namespace AoC._2025.Facts;
 
 public class Day2Test
 {
-    public static TheoryData<string, decimal[]> LineData = new()
+    public static TheoryData<string, decimal[]> LineDataPart1 = new()
     {
         { "11-22", [11m,22m] },
         { "95-115", [99m] },
@@ -20,14 +20,38 @@ public class Day2Test
         { "2121212118-2121212124", [] },
     };
 
+    public static TheoryData<string, decimal[]> LineDataPart2 = new()
+    {
+        { "11-22", [11m,22m] },
+        { "95-115", [99m, 111m] },
+        { "998-1012", [999m, 1010m] },
+        { "1188511880-1188511890", [1188511885m] },
+        { "222220-222224", [222222m] },
+        { "1698522-1698528", [] },
+        { "446443-446449", [446446] },
+        { "38593856-38593862", [38593859] },
+        { "565653-565659", [565656m] },
+        { "824824821-824824827", [824824824m] },
+        { "2121212118-2121212124", [2121212121m] },
+    };
+
+    public static TheoryData<decimal, decimal> RepetitionDataPart2 = new()
+    {
+        { 12341234, 1234 },
+        { 11, 1 },
+        { 22, 2 },
+        { 123123123, 123 },
+        { 1212121212, 12 },
+        { 1111111, 1 },
+    };
+
     [Theory]
-    [MemberData(nameof(LineData))]
+    [MemberData(nameof(LineDataPart1))]
     public void ShouldSolvePart1(string input, decimal[] expected)
     {
-        var result = SearchForDuplicatedNumber(input);
+        var result = SearchForDuplicatedNumberPart1(input);
         result.ShouldBeEquivalentTo(expected);
     }
-
 
     [Fact]
     public void ShouldSolvePart1FullSample()
@@ -42,5 +66,37 @@ public class Day2Test
     {
         var result = SolvePart1(AocInput.Day2Input);
         result.ShouldBe(18952700150m);
+    }
+
+    [Theory]
+    [MemberData(nameof(RepetitionDataPart2))]
+    public void ShouldContainRepetitionsPart2(decimal input, decimal expected)
+    {
+        var result = RepeatingNumberPart2(input);
+        result.ShouldBeEquivalentTo(expected);
+    }
+
+
+    [Theory]
+    [MemberData(nameof(LineDataPart2))]
+    public void ShouldSolvePart2(string input, decimal[] expected)
+    {
+        var result = SearchForDuplicatedNumberPart2(input);
+        result.ShouldBeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void ShouldSolvePart2FullSample()
+    {
+        var input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+        var result = SolvePart2(input);
+        result.ShouldBe(4174379265m);
+    }
+
+    [Fact]
+    public void ShouldFIndPart2Solution()
+    {
+        var result = SolvePart2(AocInput.Day2Input);
+        result.ShouldBe(28858486244m);
     }
 }
--- AoC.2025/Day2.cs
+++ AoC.2025/Day2.cs
@@ -7,30 +7,66 @@ public static class Day2
     public static decimal SolvePart1(string input)
     {
         return input.Split(",")
-            .SelectMany(SearchForDuplicatedNumber)
+            .SelectMany(SearchForDuplicatedNumberPart1)
             .Sum();
     }
 
-    public static decimal[] SearchForDuplicatedNumber(string input)
+    public static decimal[] SearchForDuplicatedNumberPart1(string input)
     {
-        var inputData = Regex.Match(input, @"(\d+)-(\d+)");
-        var lowerBound = decimal.Parse(inputData.Groups[1].Value);
-        var upperBound = decimal.Parse(inputData.Groups[2].Value);
-
-        if (lowerBound > upperBound) throw new InvalidDataException($"Strange Data in sample {input}");
-
-        var duplicatedNumbers = Enumerable
-            .Range(0, int.MaxValue)
-            .TakeWhile(x => x < upperBound - lowerBound + 1)
-            .Select(x => $"{lowerBound + x}")
-            .Where(IsRepeatingString)
-            .Select(decimal.Parse)
+        var duplicatedNumbers = ExpandNumbers(input)
+            .Where(x => IsRepeatingStringPart1($"{x}"))
             .ToArray();
 
         return duplicatedNumbers;
     }
 
-    private static bool IsRepeatingString(string value)
+    public static decimal SolvePart2(string input)
+    {
+        return input.Split(",")
+            .SelectMany(SearchForDuplicatedNumberPart2)
+            .Sum();
+    }
+
+    public static decimal[] SearchForDuplicatedNumberPart2(string input)
+    {
+        var duplicatedNumbers = ExpandNumbers(input)
+            .Where(x => RepeatingNumberPart2(x) is not null)
+            .ToArray();
+
+        return duplicatedNumbers;
+    }
+
+    public static decimal? RepeatingNumberPart2(decimal value)
+    {
+        var valueAsString = $"{value}";
+        var viableSplits = Enumerable
+            .Range(1, valueAsString.Length / 2)
+            .Where(range => valueAsString.Length % range == 0)
+            .Select(range => valueAsString[..range])
+            .Where(sample => valueAsString.Replace(sample, string.Empty).Length == 0);
+
+        return viableSplits
+            .OrderBy(sample => sample.Length)
+            .Select(decimal.Parse)
+            .OfType<decimal?>()
+            .FirstOrDefault();
+    }
+
+    private static IEnumerable<decimal> ExpandNumbers(string input)
+    {
+        var inputData = Regex.Match(input, @"(\d+)-(\d+)");
+        var lowerBound = decimal.Parse(inputData.Groups[1].Value);
+        var upperBound = decimal.Parse(inputData.Groups[2].Value);
+
+        var expandedNumbers = Enumerable
+            .Range(0, int.MaxValue)
+            .TakeWhile(x => x < upperBound - lowerBound + 1)
+            .Select(x => lowerBound + x);
+        return expandedNumbers;
+    }
+
+
+    private static bool IsRepeatingStringPart1(string value)
     {
         return value.Length % 2 == 0
                && value
