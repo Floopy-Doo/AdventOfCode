b800588df0dcaaa4302ca9b504c4c61db19f3e29
Floopy-Doo <florian.bucher@hotmail.com>
2025-12-08 19:48:44 +0100

    Advent of Code 2025 - Day 7-2

--- AoC.2025.Facts/Advent-1st/Day7Test.cs
+++ AoC.2025.Facts/Advent-1st/Day7Test.cs
@@ -8,11 +8,22 @@ public class Day7Test
     public static TheoryData<string, decimal> SampleDataPart1 = new()
     {
         { ".S.\n", 0 },
-        { ".S.\n..\n.", 0 },
+        { ".S.\n...\n", 0 },
         { ".S.\n...\n.^.\n", 1 },
         { ".S.\n.^.\n", 1 },
         { ".S.\n...\n.^.\n^.^\n", 3 },
         { ".S.\n...\n.^.\n.^.\n", 1 },
+        { "..S..\n..^..\n.^.^.\n^^^^^\n", 6 },
+    };
+
+    public static TheoryData<string, decimal> SampleDataPart2 = new()
+    {
+        { ".S.\n", 1 },
+        { ".S.\n...\n", 1 },
+        { ".S.\n...\n.^.\n", 2 },
+        { ".S.\n.^.\n", 2 },
+        { ".S.\n...\n.^.\n^.^\n", 2 },
+        { ".S.\n...\n.^.\n.^.\n", 2 },
         { "..S..\n..^..\n.^.^.\n^^^^^\n", 6 },
     };
 
@@ -37,5 +48,28 @@ public class Day7Test
     {
         var result = SolvePart1(AocInput.Day7Input);
         result.ShouldBe(1675);
+    }
+
+    [Theory]
+    [MemberData(nameof(SampleDataPart2))]
+    public void ShouldSolvePar2(string input, decimal expected)
+    {
+        var result = SolvePart2(input);
+        result.ShouldBeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void ShouldSolvePart2FullSample()
+    {
+        var input = ".......S.......\n...............\n.......^.......\n...............\n......^.^......\n...............\n.....^.^.^.....\n...............\n....^.^...^....\n...............\n...^.^...^.^...\n...............\n..^...^.....^..\n...............\n.^.^.^.^.^...^.\n...............";
+        var result = SolvePart2(input);
+        result.ShouldBe(40);
+    }
+
+    [Fact]
+    public void ShouldFindPart2Solution()
+    {
+        var result = SolvePart2(AocInput.Day7Input);
+        result.ShouldBe(187987920774390m);
     }
 }
--- AoC.2025/Advent-1st/Day7.cs
+++ AoC.2025/Advent-1st/Day7.cs
@@ -6,7 +6,7 @@ public static class Day7
 {
     public static decimal SolvePart1(string input)
     {
-        var lines = input.Split("\n");
+        var lines = input.Split("\n", StringSplitOptions.RemoveEmptyEntries);
         var initialBeams = lines[0].Select(IsBeam);
 
         var state = lines[1..].Aggregate(
@@ -14,53 +14,74 @@ public static class Day7
             EvaluateBeamPropagation);
 
         return state.TotalSplits;
+    }
 
-        static State EvaluateBeamPropagation(State state, string line)
-        {
-            var fullPropagation = line.Zip(state.ActiveBeams)
-                .SelectMany(ApplyBeamCalculation)
-                .ToList();
+    public static decimal SolvePart2(string input)
+    {
+        var lines = input.Split("\n", StringSplitOptions.RemoveEmptyEntries);
+        var initialBeams = lines[0].Select(IsBeam);
 
-            var newBeams = fullPropagation.GroupBy(x => x.Index,
-                    (i, group) => (Index: i,
-                        WithBeam: group.Aggregate(false, (acc, data) => acc || data is not NoBeam)))
-                .Where(x => x.Index >= 0 && x.Index < line.Length)
-                .OrderBy(x => x.Index)
-                .Select(x => x.WithBeam)
-                .ToArray();
+        var state = lines[1..].Aggregate(
+            new State([.. initialBeams], 0),
+            EvaluateBeamPropagation);
 
-            var newSplits = fullPropagation.Count(x => x is SplitBeam) / 2;
-            return new State(newBeams, state.TotalSplits + newSplits);
-        }
+        return state.ParticlesInBeams.Sum();
+    }
+
+
+    private static State EvaluateBeamPropagation(State state, string line)
+    {
+        var fullPropagation = line.Zip(state.ParticlesInBeams)
+            .SelectMany(ApplyBeamCalculation)
+            .ToList();
+
+        var newBeams = fullPropagation
+            .GroupBy(x => x.Index,
+                (i, group) =>
+                (
+                    Index: i,
+                    ActiveParticles: group.Aggregate(0m, (acc, data) => acc + data.ActiveParticles))
+            )
+            .Where(x => x.Index >= 0 && x.Index < line.Length)
+            .OrderBy(x => x.Index)
+            .Select(x => x.ActiveParticles)
+            .ToArray();
+
+        var newSplits = fullPropagation.Count(x => x is SplitBeam) / 2;
+        return new State(newBeams, state.TotalSplits + newSplits);
     }
 
     private static IEnumerable<Propagation> ApplyBeamCalculation(
-        (char input, bool hasActiveBeam) data,
+        (char Input, decimal ActiveParticles) data,
         int index)
     {
-        return (data.hasActiveBeam, data.input) switch
+        return (data.ActiveParticles, data.Input) switch
         {
-            (true, '^') =>
-                [new SplitBeam(index - 1), new NoBeam(index), new SplitBeam(index + 1)],
-            (true, '.') =>
-                [new Beam(index)],
+            (> 0, '^') =>
+            [
+                new SplitBeam(index - 1, data.ActiveParticles),
+                new NoBeam(index),
+                new SplitBeam(index + 1, data.ActiveParticles),
+            ],
+            (> 0, '.') =>
+                [new Beam(index, data.ActiveParticles)],
             _ => [new NoBeam(index)],
         };
     }
 
-    private static bool IsBeam(char input)
+    private static decimal IsBeam(char input)
     {
-        return input == 'S';
+        return input == 'S' ? 1 : 0;
     }
 
-    private sealed record State(bool[] ActiveBeams, decimal TotalSplits);
+    private sealed record State(decimal[] ParticlesInBeams, decimal TotalSplits);
 
-    public abstract record Propagation(int Index)
+    public abstract record Propagation(int Index, decimal ActiveParticles)
     {
-        public sealed record Beam(int Index) : Propagation(Index);
+        public sealed record Beam(int Index, decimal ActiveParticles) : Propagation(Index, ActiveParticles);
 
-        public sealed record SplitBeam(int Index) : Propagation(Index);
+        public sealed record SplitBeam(int Index, decimal ActiveParticles) : Propagation(Index, ActiveParticles);
 
-        public sealed record NoBeam(int Index) : Propagation(Index);
+        public sealed record NoBeam(int Index) : Propagation(Index, 0m);
     }
 }
